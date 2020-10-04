6958f9107fb705e9fe31d60a6c82a2566345d42a
Ivan Kalagin <ivan.kalagin@inplat.ru>
2020-10-04 21:44:04 +0300

    Init repo for fp practice

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  target: 'web',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        loader: 'awesome-typescript-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+};
